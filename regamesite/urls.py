4e55dc3fc71480c9376204569b9748382a542805
Piotr Wójcik <chocimier@tlen.pl>
2019-03-10 17:32:36 +0100

    Login redirections

--- regamesite/urls.py
+++ regamesite/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django_registration.backends.one_step.views import RegistrationView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/register/', RegistrationView.as_view(success_url='/'), name='django_registration_register'),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/', include('django_registration.backends.one_step.urls')),
     path('', include('regame.urls')),
 ]
