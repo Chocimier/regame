902557bf453c5534574742ccd47c7b3982393ed3
Piotr Wójcik <chocimier@tlen.pl>
2019-03-11 21:36:43 +0100

    Match view stub

--- regame/urls.py
+++ regame/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('', views.main, name='main')
+   path('', views.main, name='main'),
+   path('match/<int:no>', views.match, name='match'),
 ]
--- regame/views.py
+++ regame/views.py
@@ -1,10 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 def challenge():
     return render(request, 'regame/challenge.html')
 
-def match():
-    pass
+def match(request, no):
+    return HttpResponse(str(no))
 
 def main(request):
     return render(request, 'regame/main.html')
