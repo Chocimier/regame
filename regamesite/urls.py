bb692b85f2ac7e117e48e2366ae9b7acbde6ab3a
Piotr Wójcik <chocimier@tlen.pl>
2019-03-25 18:18:28 +0100

    Drop admin pages

--- regamesite/urls.py
+++ regamesite/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
 from django_registration.backends.one_step.views import RegistrationView
@@ -21,7 +20,6 @@ from regame.forms import NoEmailUserForm
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('accounts/register/', RegistrationView.as_view(
             success_url='/',
             form_class=NoEmailUserForm),
