7a19af3539205f0bc97509666d774b270dc3ee8b
Piotr Wójcik <chocimier@tlen.pl>
2019-03-26 17:20:58 +0100

    Add management command for reserving usernames

---
+++ regame/management/commands/overtakeusernames.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+from django.db.utils import IntegrityError
+
+
+class Command(BaseCommand):
+    help = 'Overtakes usernames that should be not available for players'
+
+    def handle(self, *args, **options):
+        reserved = [
+            'a stranger',
+            'admin',
+            'bot',
+            'stranger',
+            'you',
+            'your',
+        ]
+        for username in reserved:
+            try:
+                get_user_model().objects.create_user(username=username)
+            except IntegrityError:
+                pass
