ed008732436abf565b04db00c9cb54d18c74fe7b
Piotr Wójcik <chocimier@tlen.pl>
2019-03-26 18:56:21 +0100

    Heroku settings

--- regamesite/settings.py
+++ regamesite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = ''
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
@@ -128,3 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+if 'DYNO' in os.environ:
+    import dj_database_url
+    import django_heroku
+
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+    django_heroku.settings(locals())
