60b04eb54757b4af08f8a8d36f6d97ed10a850c2
Piotr Wójcik <chocimier@tlen.pl>
2019-03-26 20:27:18 +0100

    Fix JS bug: refill with selected card

--- regame/static/regame/scripts/match.js
+++ regame/static/regame/scripts/match.js
@@ -1,7 +1,10 @@
 (function () {
-    var cards = document.querySelectorAll('label > .card')
+    var cards = document.querySelectorAll('.yourtablerow label .card')
     for (var i = 0; i < cards.length; ++i) {
-        cards[i].addEventListener('click', function (ev, x, d) {
+        cards[i].addEventListener('click', function (ev) {
+            var isEmpty = function(obj) {
+                return Object.keys(obj).length == 0
+            }
             var control = this.parentNode.control
             if (!control) {
                 return
@@ -10,10 +13,13 @@
                 control.value = '0'
                 return
             }
-            var controls = control.form.querySelectorAll('[name^="on_table_"]')
+            var controls = document.querySelectorAll('.yourtablerow [name^="on_table_"]')
             var used = {};
             for (var i = 0; i < controls.length; ++i) {
                 used[controls[i].value] = true
+            }
+            if (isEmpty(used)){
+                return
             }
             for (var i = 1; i <= controls.length; ++i) {
                 if (!used[i]) {
